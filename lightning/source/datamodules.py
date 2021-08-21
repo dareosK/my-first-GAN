3d27be9f5dea4b68a76536815df2503d00efba26
greggyfromtheblock <greg.pascalis@gmail.com>
2021-08-21 14:51:42 +0200

    add method get_batch in the class

--- lightning/source/datamodules.py
+++ lightning/source/datamodules.py
@@ -2,13 +2,13 @@ import pytorch_lightning as pl
 
 import torchvision.datasets as TorchvisionDataset
 import matplotlib.pyplot as plt
-import torchvision.transforms as transforms
+import torchvision.transforms as T
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
 import torchvision.utils as TorchVisionUtils
 
 
-class WaifuDataModule(pl.LightningDataModule):
+class WaifuDatamodule(pl.LightningDataModule):
     def __init__(self, folderpath, batch_size=128, image_size=64, transforms=None):
         """"
         :parameter folderpath: folder containing waifu images
@@ -20,33 +20,36 @@ class WaifuDataModule(pl.LightningDataModule):
         self.batch_size = batch_size
         self.image_size = image_size
         self.transforms =  transforms if transforms is not None else \
-                           transforms.Compose([transforms.Resize(image_size),
-                           transforms.CenterCrop(image_size),
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+                           T.Compose([T.Resize(image_size),
+                           T.CenterCrop(image_size),
+                           T.ToTensor(),
+                           T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
         self.dataset = TorchvisionDataset.ImageFolder(root=folderpath,
                                                       transform=self.transforms)
-        self.data = None
+        self.dataloader = None
 
     def setup(self, stage=None):
-        self.data = DataLoader(self.dataset, batch_size=self.batch_size,
+        self.dataloader = DataLoader(self.dataset, batch_size=self.batch_size,
                                      shuffle=True, num_workers=4)
 
-    def preview_batch(self):
+    def get_batch(self):
+        if self.dataloader is None:
+            self.setup()
+        # The syntax is weird but this is how you get a batch from dataloader
+        batch = next(iter(self.dataloader))
+        return batch
+
+    def preview_batch(self, normalize=True):
         """
         Preview a batch of waifus
-        :return: None
         """
-        if self.data is None:
-            self.setup()
-        # Get batch
-        batch = next(iter(self.dataloader))
+        batch = self.get_batch()
         plt.figure(figsize=(8, 8))
         plt.axis("off")
         plt.title("Training Images")
         plt.imshow(np.transpose(TorchVisionUtils.make_grid(batch[0].to('cpu')[:64],
                                                            padding=2,
-                                                           normalize=True).cpu(), (1, 2, 0)))
+                                                           normalize=normalize).cpu(), (1, 2, 0)))
 
 
